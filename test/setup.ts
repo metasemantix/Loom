fb0117085e5081152910e07f746b973ad697c29f
metasemantix <t.juengling@web.de>
2026-08-12 18:45:55 +0200

    Aktualisieren von setup.ts

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,9 @@
 import { env } from "cloudflare:workers";
 import migration from "../migrations/0001_initial.sql?raw";
 
-await env.DB.exec(migration);
+for (const statement of migration
+  .split(";")
+  .map((part) => part.trim())
+  .filter(Boolean)) {
+  await env.DB.prepare(statement).run();
+}
